67442b86639bacc6f186182f726292b172eed6b6
Martin Schürrer <martin@schuerrer.org>
2015-09-28 13:31:00 +0200

    Support Elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LoggerFileBackend.Mixfile do
   def project do
     [app: :logger_file_backend,
      version: "0.0.4",
-     elixir: "~> 1.0.0",
+     elixir: ">= 1.0.0 and <= 1.2.0",
      description: description,
      package: package,
      deps: deps]
