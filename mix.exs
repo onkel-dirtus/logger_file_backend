b2fd3b6e82da195ecb2c063736c5a90c0d7a299c
Victor Rodrigues <vitumail@gmail.com>
2016-09-07 20:09:48 +0200

    Bump to 0.0.9

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LoggerFileBackend.Mixfile do
 
   def project do
     [app: :logger_file_backend,
-     version: "0.0.9-dev",
+     version: "0.0.9",
      elixir: "~> 1.0",
      description: description,
      package: package,
