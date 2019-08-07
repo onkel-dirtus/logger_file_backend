36644bab69f1bde29bcb9d9c4a108ea6e6d66df8
Kurt Williams <kurt.r.williams@gmail.com>
2019-08-06 23:16:46 -0500

    Bump version to 0.0.11

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LoggerFileBackend.Mixfile do
 
   def project do
     [app: :logger_file_backend,
-     version: "0.0.10",
+     version: "0.0.11",
      elixir: "~> 1.0",
      description: description(),
      package: package(),
